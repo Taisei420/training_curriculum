d0fba2f84b6163ef18a693d1f6a5b0fb407c3bb3
Taisei420 <taiseiblog96@gmail.com>
2020-11-09 18:32:05 +0900

    Revert "Merge pull request #8 from Taisei420/second_training"
    This reverts commit d6f5da10bca37cc8c00a064d8270a4bc0afaa21d, reversing
    changes made to 77cd4e259e4cceb235ec90fba8f37020d209d560.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -34,9 +33,7 @@ class CalendarsController < ApplicationController
       today_plans = []
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
- 
-      
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wday:}
+      end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
